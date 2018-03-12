8118d086af698efa2a199bc124249042e7f2212f
fukuhara mizuki <mizuki.as2o.0607@gmail.com>
2018-03-12 17:48:10 +0900

    Q9.sql

---
+++ Q1.sql
@@ -0,0 +1,1 @@
+CREATE TABLE item_category(category_id SERIAL,category_name VAR CHAR(256)NOT NULL);
---
+++ Q4.sql
@@ -0,0 +1,8 @@
+INSERT INTO item(item_name,item_price,category_id)VALUES('堅牢な机',3000,1);
+INSERT INTO item(item_name,item_price,category_id)VALUES('生焼け肉',50,2);
+INSERT INTO item(item_name,item_price,category_id)VALUES('すっきりわかるJAVA入門',3000,3);
+INSERT INTO item(item_name,item_price,category_id)VALUES('おしゃれな椅子',2000,1);
+INSERT INTO item(item_name,item_price,category_id)VALUES('こんがり肉',500,2);
+INSERT INTO item(item_name,item_price,category_id)VALUES('書き方ドリルSQL',2500,3);
+INSERT INTO item(item_name,item_price,category_id)VALUES('ふわふわのベッド',30000,1);
+INSERT INTO item(item_name,item_price,category_id)VALUES('ミラノ風ドリア',300,2);
